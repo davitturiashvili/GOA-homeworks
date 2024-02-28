354fc911c4c67f47d9fb23f6eda1317a33c7540e
davitturiashvili <datunaturiashvili777@gmail.com>
2024-02-28 23:58:29 +0400

    second commit

---
+++ Day 1/Classwork/classwork.py
@@ -0,0 +1,34 @@
+print(10 / 3) # მიბრუნებს ზუსტ გაყოფის შედეგს
+print(5 // 2) # მიბრუნებს შედეგს თუ რამდენჯერ მოთავსდება გამყოფი გასაყოფში
+print(5 % 2) # მიბრუნებს ნაშთს
+
+print("davit" * 3) # გამრავლდება სამჯერ
+
+# print(5 * 3.1)
+# print(5 / 3.1)
+
+# print(5 > 3) # true
+# print(5 < 3) # false
+# print("5 > 3") 
+# print(5 + 3) # დააბრუნებს 8
+# print("5 + 3") # დააბრუნებს 5 + 3
+
+
+num1 = input("Please enter first number:")
+num2 = int(input("Please enter second number"))
+
+print(float(num1) + 3)
+print(str(num2) + "5")
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
---
+++ Day 3/homework1.py
@@ -0,0 +1,8 @@
+print (hi)
+print_(hell)o
+print(how are you)
+print        why you are getting mad
+dont print(i said to rest noob)
+print( 5 +<-*==//)
+print               how to error pls help mee 🙏🙏🙏
+pow you miss GOA lesson:
